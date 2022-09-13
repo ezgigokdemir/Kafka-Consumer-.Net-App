e5a8ec8a9c09313e33339080b3eacb38b11151e9
Eylem GÖKDEMİR <gokdemirezgi7@gmail.com>
2022-09-14 00:50:23 +0300

    push notifications

--- KafkaConsumerAPI/Services/CouchbaseService.cs
+++ KafkaConsumerAPI/Services/CouchbaseService.cs
@@ -3,8 +3,6 @@ using Couchbase.Extensions.DependencyInjection;
 using KafkaConsumerAPI.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace KafkaConsumerAPI.Services
 {
